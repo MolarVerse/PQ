ca29ea37379593a016817c5be1b64beb304828b4
97gamjak <97gamjak@gmail.com>
2023-06-05 10:43:35 +0200

    enfile output reformatted

--- src/output/energyOutput.cpp
+++ src/output/energyOutput.cpp
@@ -6,23 +6,57 @@ using namespace std;
 
 void EnergyOutput::write(const size_t step, const PhysicalData &data)
 {
-    _fp << step << "\t";
+    _fp << right;
+    _fp << setw(10);
+    _fp << step;
+
     _fp << fixed;
     _fp << setprecision(12);
-    _fp << data.getTemperature() << "\t";
-    _fp << data.getPressure() << "\t";
-    _fp << 0.0 << "\t";
-    _fp << data.getKineticEnergy() << "\t";
-    _fp << 0.0 << "\t";
-    _fp << data.getCoulombEnergy() << "\t";
-    _fp << data.getNonCoulombEnergy() << "\t";
-    _fp << 0.0 << "\t";
-    _fp << 0.0 << "\t";
+
+    _fp << right;
+    _fp << setw(20);
+    _fp << data.getTemperature();
+
+    _fp << right;
+    _fp << setw(20);
+    _fp << data.getPressure();
+
+    _fp << right;
+    _fp << setw(20);
+    _fp << 0.0;
+
+    _fp << right;
+    _fp << setw(20);
+    _fp << data.getKineticEnergy();
+
+    _fp << right;
+    _fp << setw(20);
+    _fp << 0.0;
+
+    _fp << right;
+    _fp << setw(20);
+    _fp << data.getCoulombEnergy();
+
+    _fp << right;
+    _fp << setw(20);
+    _fp << data.getNonCoulombEnergy();
+
+    _fp << right;
+    _fp << setw(20);
+    _fp << 0.0;
+
+    _fp << right;
+    _fp << setw(20);
+    _fp << 0.0;
 
     _fp << scientific;
-    _fp << data.getMomentum() << "\t";
+    _fp << right;
+    _fp << setw(20);
+    _fp << data.getMomentum();
 
     _fp << fixed;
-    _fp << 0.0 << "\t";
+    _fp << right;
+    _fp << setw(20);
+    _fp << 0.0;
     _fp << endl;
 }
