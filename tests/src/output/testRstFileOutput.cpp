d4818351a57d6b48edf5440286084a3a8c6e040e
Jakob Gamper <97gamjak@gmail.com>
2023-07-02 23:58:49 +0200

    rstFileoutput tested

---
+++ tests/src/output/testRstFileOutput.cpp
@@ -0,0 +1,33 @@
+#include "testRstFileOutput.hpp"
+
+TEST_F(TestRstFileOutput, write)
+{
+    _rstFileOutput->setFilename("default.rst");
+    _rstFileOutput->write(*_simulationBox, 10);
+    _rstFileOutput->close();
+    std::ifstream file("default.rst");
+    std::string   line;
+    getline(file, line);
+    EXPECT_EQ(line, "Step 10");
+    getline(file, line);
+    EXPECT_EQ(line, "Box   10 10 10  90 90 90");
+    getline(file, line);
+    EXPECT_EQ(line,
+              "H    1    1         1.00000000     1.00000000     1.00000000      1.00000000e+00      1.00000000e+00      "
+              "1.00000000e+00     1.00000000     1.00000000     1.00000000");
+    getline(file, line);
+    EXPECT_EQ(line,
+              "O    2    1         1.00000000     2.00000000     3.00000000      3.00000000e+00      4.00000000e+00      "
+              "5.00000000e+00     2.00000000     3.00000000     4.00000000");
+
+    getline(file, line);
+    EXPECT_EQ(line,
+              "Ar   1    2         1.00000000     1.00000000     1.00000000      1.00000000e+00      1.00000000e+00      "
+              "1.00000000e+00     1.00000000     1.00000000     1.00000000");
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return ::RUN_ALL_TESTS();
+}
