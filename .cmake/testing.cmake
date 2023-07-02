6ce8419c54fc33168b872422fce8eb9a59bf8bdd
Jakob Gamper <97gamjak@gmail.com>
2023-07-02 23:17:18 +0200

    testing.cmake updated

--- .cmake/testing.cmake
+++ .cmake/testing.cmake
@@ -17,7 +17,7 @@ if(NOT ${GTest_FOUND})
     FetchContent_MakeAvailable(gtest)
 endif()
 
-if(${CMAKE_BUILD_TYPE} MATCHES "^[Dd]ebug" OR ${CMAKE_BUILD_TYPE} MATCHES "^[Rr]elWithDebug")
+if(${CMAKE_BUILD_TYPE} MATCHES "^[Dd]ebug")
     include(gcovr)
 
     list(APPEND CMAKE_CTEST_ARGUMENTS "--output-on-failure")
