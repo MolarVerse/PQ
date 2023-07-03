36f2481ef92f90e7626ef5b90008b5888a1ca0b6
97gamjak <97gamjak@gmail.com>
2023-07-03 23:53:21 +0200

    codecov.yaml and testing.cmake updated

--- tests/src/manostat/testManostat.cpp
+++ tests/src/manostat/testManostat.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-TEST_F(TestManostat, testCalculatePressure)
+TEST_F(TestManostat, CalculatePressure)
 {
     _manostat.calculatePressure(*_data);
 
     EXPECT_DOUBLE_EQ(_data->getPressure(), 3.0 * config::_PRESSURE_FACTOR_);
 }
 
-TEST_F(TestManostat, testChangeVirialToAtomic)
+TEST_F(TestManostat, ChangeVirialToAtomic)
 {
     _data->changeKineticVirialToAtomic();
 
@@ -22,13 +22,18 @@ TEST_F(TestManostat, testChangeVirialToAtomic)
 
 TEST_F(TestManostat, testApplyManostat)
 {
+    _manostat = manostat::BerendsenManostat(1.0, 0.1);
     _manostat.applyManostat(*_box, *_data);
+
+    const auto old_pressure = 2.0 * config::_PRESSURE_FACTOR_;
 
     EXPECT_DOUBLE_EQ(_data->getPressure(), 3.0 * config::_PRESSURE_FACTOR_);
 }
 
+TEST_F(TestManostat, applyManostatBerendsen) {}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    return ::RUN_ALL_TESTS();
 }
