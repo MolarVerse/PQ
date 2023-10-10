dd4d0a5b7358bec7ee83413ea0cd1ceb925949ff
Jakob Gamper <97gamjak@gmail.com>
2023-10-10 23:34:22 +0200

    number of qm atoms added to en file

--- src/output/energyOutput.cpp
+++ src/output/energyOutput.cpp
@@ -60,7 +60,7 @@ void EnergyOutput::write(const size_t step, const double loopTime, const physica
     if (settings::Settings::isQMActivated())
     {
         _fp << std::format("{:20.12f}\t", data.getQMEnergy());
-        _fp << std::format("{:20.12f}\t", 0.0);   // TODO: implement
+        _fp << std::format("{:20.12f}\t", data.getNumberOfQMAtoms());
     }
 
     if (settings::Settings::isRingPolymerMDActivated())
