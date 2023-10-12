191642ad242c35c97f48690284c7ac98b86fd8c8
97gamjak <97gamjak@gmail.com>
2023-10-12 12:35:28 +0200

    executables now working for python tools

--- tools/python/setup.py
+++ tools/python/setup.py
@@ -27,5 +27,9 @@ from setuptools import setup, find_packages
 setup(
     name='pimd_qmcf_tools',
     version='0.1.0',
-    packages=find_packages(include=['pimd_qmcf_tools'])
+    packages=find_packages(include=['pimd_qmcf_tools']),
+    scripts=[
+        "bin/traj2box",
+        "bin/traj2qmcfc"
+    ]
 )
