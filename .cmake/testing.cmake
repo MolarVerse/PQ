33ceb8a04fa5ab1f8b4b3d5c66f91d614dcd1270
97gamjak <97gamjak@gmail.com>
2023-11-08 07:34:43 +0100

    updated CMakeLists.txt to add submodule

--- .cmake/testing.cmake
+++ .cmake/testing.cmake
@@ -1,24 +1,26 @@
 enable_testing()
 option(INSTALL_GMOCK "install Googletest's GMock?" OFF)
 option(INSTALL_GTEST "install Googletest's GTest?" OFF)
-find_package(GTest CONFIG) # use googletest
 
-if(NOT ${GTest_FOUND})
-    message("GTest not found. Fetching...")
-    include(FetchContent)
-    FetchContent_Declare(
-        gtest
-        GIT_REPOSITORY "https://github.com/google/googletest"
-        GIT_TAG main
-    )
+find_package(GIT QUIET)
 
-    # For Windows: Prevent overriding the parent project's compiler/linker settings
-    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
-    FetchContent_MakeAvailable(gtest)
-else()
-    MESSAGE(STATUS "aaa ${gtest_SOURCE_DIR}")
-    include_directories(${GTEST_SOURCE_DIR}/include ${GMOCK_SOURCE_DIR}/include ${GTEST_SOURCE_DIR} ${GMOCK_SOURCE_DIR})
+if(GIT_FOUND AND EXISTS "${PROJECT_SOURCE_DIR}/.git")
+    # Update submodules as needed
+    option(GIT_SUBMODULE "Check submodules during build" ON)
+
+    if(GIT_SUBMODULE)
+        message(STATUS "Submodule update")
+        execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            RESULT_VARIABLE GIT_SUBMOD_RESULT)
+
+        if(NOT GIT_SUBMOD_RESULT EQUAL "0")
+            message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
+        endif()
+    endif()
 endif()
+
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
 list(APPEND CMAKE_CTEST_ARGUMENTS "--output-on-failure")
 
