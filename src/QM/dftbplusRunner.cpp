5017a0d690dc618d85000ae2e65301b78fc558b6
Jakob Gamper <97gamjak@gmail.com>
2024-04-18 01:35:22 +0200

    stress tensor written to file is now in units of PQ

--- src/QM/dftbplusRunner.cpp
+++ src/QM/dftbplusRunner.cpp
@@ -132,7 +132,7 @@ void DFTBPlusRunner::readStressTensor(simulationBox::Box &box, physicalData::Phy
 
     const auto virial = stressTensor * constants::_HARTREE_PER_BOHR_CUBIC_TO_KCAL_PER_MOL_PER_ANGSTROM_CUBIC_ * box.getVolume();
 
-    data.setStressTensor(stressTensor);
+    data.setStressTensor(stressTensor * constants::_HARTREE_PER_BOHR_CUBIC_TO_KCAL_PER_MOL_PER_ANGSTROM_CUBIC_);
     data.addVirial(virial);
 
     stressFile.close();
