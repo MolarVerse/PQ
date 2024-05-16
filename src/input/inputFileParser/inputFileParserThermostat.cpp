003a8b1f290e2c94219d372658472a86fdd5e2e7
Jakob Gamper <97gamjak@gmail.com>
2024-05-16 02:48:17 +0200

    introduced new keywords in order to perform tepmerature ramping

--- src/input/inputFileParser/inputFileParserThermostat.cpp
+++ src/input/inputFileParser/inputFileParserThermostat.cpp
@@ -22,41 +22,95 @@
 
 #include "inputFileParserThermostat.hpp"
 
+#include <cstddef>       // for size_t, std
+#include <format>        // for format
+#include <functional>    // for _Bind_front_t, bind_front
+#include <string_view>   // for string_view
+
 #include "exceptions.hpp"           // for InputFileException, customException
 #include "references.hpp"           // for References
 #include "referencesOutput.hpp"     // for ReferencesOutput
 #include "stringUtilities.hpp"      // for toLowerCopy
 #include "thermostatSettings.hpp"   // for ThermostatSettings
 
-#include <cstddef>       // for size_t, std
-#include <format>        // for format
-#include <functional>    // for _Bind_front_t, bind_front
-#include <string_view>   // for string_view
-
 using namespace input;
 
 /**
- * @brief Construct a new Input File Parser Thermostat:: Input File Parser Thermostat object
+ * @brief Construct a new Input File Parser Thermostat:: Input File Parser
+ * Thermostat object
  *
- * @details following keywords are added to the _keywordFuncMap, _keywordRequiredMap and _keywordCountMap:
- * 1) thermostat <string>
- * 2) temp <double>
- * 3) t_relaxation <double>
- * 4) friction <double>
- * 5) nh-chain_length <size_t>
- * 6) coupling_frequency <double>
+ * @details following keywords are added to the _keywordFuncMap,
+ * _keywordRequiredMap and _keywordCountMap: 1) thermostat <string> 2) temp
+ * <double> 3) t_relaxation <double> 4) friction <double> 5) nh-chain_length
+ * <size_t> 6) coupling_frequency <double>
  *
  * @param engine
  */
-InputFileParserThermostat::InputFileParserThermostat(engine::Engine &engine) : InputFileParser(engine)
+InputFileParserThermostat::InputFileParserThermostat(engine::Engine &engine)
+    : InputFileParser(engine)
 {
-    addKeyword(std::string("thermostat"), bind_front(&InputFileParserThermostat::parseThermostat, this), false);
-    addKeyword(std::string("temp"), bind_front(&InputFileParserThermostat::parseTemperature, this), false);
-    addKeyword(std::string("t_relaxation"), bind_front(&InputFileParserThermostat::parseThermostatRelaxationTime, this), false);
-    addKeyword(std::string("friction"), bind_front(&InputFileParserThermostat::parseThermostatFriction, this), false);
-    addKeyword(std::string("nh-chain_length"), bind_front(&InputFileParserThermostat::parseThermostatChainLength, this), false);
     addKeyword(
-        std::string("coupling_frequency"), bind_front(&InputFileParserThermostat::parseThermostatCouplingFrequency, this), false);
+        std::string("thermostat"),
+        bind_front(&InputFileParserThermostat::parseThermostat, this),
+        false
+    );
+    addKeyword(
+        std::string("temp"),
+        bind_front(&InputFileParserThermostat::parseTemperature, this),
+        false
+    );
+    addKeyword(
+        std::string("start_temp"),
+        bind_front(&InputFileParserThermostat::parseTemperature, this),
+        false
+    );
+    addKeyword(
+        std::string("end_temp"),
+        bind_front(&InputFileParserThermostat::parseTemperature, this),
+        false
+    );
+    addKeyword(
+        std::string("temperature_ramp_steps"),
+        bind_front(&InputFileParserThermostat::parseTemperatureRampSteps, this),
+        false
+    );
+    addKeyword(
+        std::string("temperature_ramp_frequency"),
+        bind_front(
+            &InputFileParserThermostat::parseTemperatureRampFrequency,
+            this
+        ),
+        false
+    );
+    addKeyword(
+        std::string("t_relaxation"),
+        bind_front(
+            &InputFileParserThermostat::parseThermostatRelaxationTime,
+            this
+        ),
+        false
+    );
+    addKeyword(
+        std::string("friction"),
+        bind_front(&InputFileParserThermostat::parseThermostatFriction, this),
+        false
+    );
+    addKeyword(
+        std::string("nh-chain_length"),
+        bind_front(
+            &InputFileParserThermostat::parseThermostatChainLength,
+            this
+        ),
+        false
+    );
+    addKeyword(
+        std::string("coupling_frequency"),
+        bind_front(
+            &InputFileParserThermostat::parseThermostatCouplingFrequency,
+            this
+        ),
+        false
+    );
 }
 
 /**
@@ -70,9 +124,13 @@ InputFileParserThermostat::InputFileParserThermostat(engine::Engine &engine) : I
  *
  * @param lineElements
  *
- * @throws customException::InputFileException if thermostat is not "none" or "berendsen"
+ * @throws customException::InputFileException if thermostat is not "none" or
+ * "berendsen"
  */
-void InputFileParserThermostat::parseThermostat(const std::vector<std::string> &lineElements, const size_t lineNumber)
+void InputFileParserThermostat::parseThermostat(
+    const std::vector<std::string> &lineElements,
+    const size_t                    lineNumber
+)
 {
     checkCommand(lineElements, lineNumber);
 
@@ -84,32 +142,43 @@ void InputFileParserThermostat::parseThermostat(const std::vector<std::string> &
     else if (thermostat == "berendsen")
     {
         settings::ThermostatSettings::setThermostatType("berendsen");
-        references::ReferencesOutput::addReferenceFile(references::_BERENDSEN_FILE_);
+        references::ReferencesOutput::addReferenceFile(
+            references::_BERENDSEN_FILE_
+        );
     }
 
     else if (thermostat == "velocity_rescaling" || thermostat == "rescale")
     {
         settings::ThermostatSettings::setThermostatType("velocity_rescaling");
-        references::ReferencesOutput::addReferenceFile(references::_VELOCITY_RESCALING_FILE_);
+        references::ReferencesOutput::addReferenceFile(
+            references::_VELOCITY_RESCALING_FILE_
+        );
     }
 
     else if (thermostat == "langevin")
     {
         settings::ThermostatSettings::setThermostatType("langevin");
-        references::ReferencesOutput::addReferenceFile(references::_LANGEVIN_FILE_);
+        references::ReferencesOutput::addReferenceFile(
+            references::_LANGEVIN_FILE_
+        );
     }
 
     else if (thermostat == "nh-chain")
     {
         settings::ThermostatSettings::setThermostatType("nh-chain");
-        references::ReferencesOutput::addReferenceFile(references::_NOSE_HOOVER_CHAIN_FILE_);
+        references::ReferencesOutput::addReferenceFile(
+            references::_NOSE_HOOVER_CHAIN_FILE_
+        );
     }
 
     else
-        throw customException::InputFileException(format("Invalid thermostat \"{}\" at line {} in input file. Possible options "
-                                                         "are: none, berendsen, velocity_rescaling, langevin, nh-chain",
-                                                         lineElements[2],
-                                                         lineNumber));
+        throw customException::InputFileException(format(
+            "Invalid thermostat \"{}\" at line {} in input file. Possible "
+            "options "
+            "are: none, berendsen, velocity_rescaling, langevin, nh-chain",
+            lineElements[2],
+            lineNumber
+        ));
 }
 
 /**
@@ -121,17 +190,133 @@ void InputFileParserThermostat::parseThermostat(const std::vector<std::string> &
  *
  * @throws customException::InputFileException if temperature is negative
  */
-void InputFileParserThermostat::parseTemperature(const std::vector<std::string> &lineElements, const size_t lineNumber)
+void InputFileParserThermostat::parseTemperature(
+    const std::vector<std::string> &lineElements,
+    const size_t                    lineNumber
+)
 {
     checkCommand(lineElements, lineNumber);
 
     const auto temperature = stod(lineElements[2]);
 
     if (temperature < 0)
-        throw customException::InputFileException("Temperature cannot be negative");
+        throw customException::InputFileException(
+            "Temperature cannot be negative"
+        );
 
-    settings::ThermostatSettings::setTemperatureSet(true);
     settings::ThermostatSettings::setTargetTemperature(temperature);
+}
+
+/**
+ * @brief Parse the start temperature used in the simulation
+ *
+ * @details Start temperature is needs to be set if thermostat is not "none"
+ *
+ * @param lineElements
+ *
+ * @throws customException::InputFileException if start temperature is negative
+ */
+void InputFileParserThermostat::parseStartTemperature(
+    const std::vector<std::string> &lineElements,
+    const size_t                    lineNumber
+)
+{
+    checkCommand(lineElements, lineNumber);
+
+    const auto startTemperature = stod(lineElements[2]);
+
+    if (startTemperature < 0)
+        throw customException::InputFileException(
+            "Start temperature cannot be negative"
+        );
+
+    settings::ThermostatSettings::setStartTemperature(startTemperature);
+}
+
+/**
+ * @brief Parse the end temperature used in the simulation
+ *
+ * @details End temperature is needs to be set if thermostat is not "none"
+ *
+ * @param lineElements
+ *
+ * @throws customException::InputFileException if end temperature is negative
+ */
+void InputFileParserThermostat::parseEndTemperature(
+    const std::vector<std::string> &lineElements,
+    const size_t                    lineNumber
+)
+{
+    checkCommand(lineElements, lineNumber);
+
+    const auto endTemperature = stod(lineElements[2]);
+
+    if (endTemperature < 0)
+        throw customException::InputFileException(
+            "End temperature cannot be negative"
+        );
+
+    settings::ThermostatSettings::setEndTemperature(endTemperature);
+}
+
+/**
+ * @brief Parse the temperature ramp steps used in the simulation
+ *
+ * @details if start_temp and end_temp are set, then if temperature_ramp_steps
+ * is not set, the temperature will be ramped linearly from start_temp to
+ * end_temp over the full simulation time.
+ *
+ * @param lineElements
+ *
+ * @throws customException::InputFileException if temperature ramp steps is
+ * negative
+ */
+void InputFileParserThermostat::parseTemperatureRampSteps(
+    const std::vector<std::string> &lineElements,
+    const size_t                    lineNumber
+)
+{
+    checkCommand(lineElements, lineNumber);
+
+    const auto temperatureRampSteps = stoi(lineElements[2]);
+
+    if (temperatureRampSteps < 0)
+        throw customException::InputFileException(
+            "Temperature ramp steps cannot be negative"
+        );
+
+    settings::ThermostatSettings::setTemperatureRampSteps(
+        size_t(temperatureRampSteps)
+    );
+}
+
+/**
+ * @brief Parse the temperature ramp frequency used in the simulation
+ *
+ * @details default value is 1
+ *
+ * @param lineElements
+ *
+ * @throws customException::InputFileException if temperature ramp frequency is
+ * negative
+ */
+void InputFileParserThermostat::parseTemperatureRampFrequency(
+    const std::vector<std::string> &lineElements,
+    const size_t                    lineNumber
+)
+{
+    checkCommand(lineElements, lineNumber);
+
+    const auto temperatureRampFrequency = stoi(lineElements[2]);
+
+    if (temperatureRampFrequency < 0)
+        throw customException::InputFileException(
+            "Temperature ramp frequency cannot be negative"
+        );
+
+    settings::ThermostatSettings::setTemperatureRampFrequency(
+        size_t(temperatureRampFrequency)
+    );
 }
 
 /**
@@ -143,15 +328,19 @@ void InputFileParserThermostat::parseTemperature(const std::vector<std::string> 
  *
  * @throws customException::InputFileException if relaxation time is negative
  */
-void InputFileParserThermostat::parseThermostatRelaxationTime(const std::vector<std::string> &lineElements,
-                                                              const size_t                    lineNumber)
+void InputFileParserThermostat::parseThermostatRelaxationTime(
+    const std::vector<std::string> &lineElements,
+    const size_t                    lineNumber
+)
 {
     checkCommand(lineElements, lineNumber);
 
     const auto relaxationTime = stod(lineElements[2]);
 
     if (relaxationTime < 0)
-        throw customException::InputFileException("Relaxation time of thermostat cannot be negative");
+        throw customException::InputFileException(
+            "Relaxation time of thermostat cannot be negative"
+        );
 
     settings::ThermostatSettings::setRelaxationTime(relaxationTime);
 }
@@ -165,14 +354,19 @@ void InputFileParserThermostat::parseThermostatRelaxationTime(const std::vector<
  *
  * @throws customException::InputFileException if friction is negative
  */
-void InputFileParserThermostat::parseThermostatFriction(const std::vector<std::string> &lineElements, const size_t lineNumber)
+void InputFileParserThermostat::parseThermostatFriction(
+    const std::vector<std::string> &lineElements,
+    const size_t                    lineNumber
+)
 {
     checkCommand(lineElements, lineNumber);
 
     const auto friction = stod(lineElements[2]);
 
     if (friction < 0)
-        throw customException::InputFileException("Friction of thermostat cannot be negative");
+        throw customException::InputFileException(
+            "Friction of thermostat cannot be negative"
+        );
 
     settings::ThermostatSettings::setFriction(friction * 1.0e12);
 }
@@ -186,14 +380,19 @@ void InputFileParserThermostat::parseThermostatFriction(const std::vector<std::s
  *
  * @throws customException::InputFileException if chain length is negative
  */
-void InputFileParserThermostat::parseThermostatChainLength(const std::vector<std::string> &lineElements, const size_t lineNumber)
+void InputFileParserThermostat::parseThermostatChainLength(
+    const std::vector<std::string> &lineElements,
+    const size_t                    lineNumber
+)
 {
     checkCommand(lineElements, lineNumber);
 
     const auto chainLength = stoi(lineElements[2]);
 
     if (chainLength < 0)
-        throw customException::InputFileException("Chain length of thermostat cannot be negative");
+        throw customException::InputFileException(
+            "Chain length of thermostat cannot be negative"
+        );
 
     settings::ThermostatSettings::setNoseHooverChainLength(size_t(chainLength));
 }
@@ -207,15 +406,21 @@ void InputFileParserThermostat::parseThermostatChainLength(const std::vector<std
  *
  * @throws customException::InputFileException if coupling frequency is negative
  */
-void InputFileParserThermostat::parseThermostatCouplingFrequency(const std::vector<std::string> &lineElements,
-                                                                 const size_t                    lineNumber)
+void InputFileParserThermostat::parseThermostatCouplingFrequency(
+    const std::vector<std::string> &lineElements,
+    const size_t                    lineNumber
+)
 {
     checkCommand(lineElements, lineNumber);
 
     const auto couplingFrequency = stod(lineElements[2]);
 
     if (couplingFrequency < 0)
-        throw customException::InputFileException("Coupling frequency of thermostat cannot be negative");
+        throw customException::InputFileException(
+            "Coupling frequency of thermostat cannot be negative"
+        );
 
-    settings::ThermostatSettings::setNoseHooverCouplingFrequency(couplingFrequency);
+    settings::ThermostatSettings::setNoseHooverCouplingFrequency(
+        couplingFrequency
+    );
 }
