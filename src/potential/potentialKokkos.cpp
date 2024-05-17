f0ba27614e10db98ecd4bfea30dbb0b51d655847
Josef M. Gallmetzer <gallmetzer.josef@gmail.com>
2024-05-17 13:20:05 +0200

    Deleted kokkos init in potentialkokkos

--- src/potential/potentialKokkos.cpp
+++ src/potential/potentialKokkos.cpp
@@ -22,13 +22,11 @@
 
 #include <cstddef>   // for size_t
 
-#include <cstddef>   // for size_t
-
-#include "molecule.hpp"       // for Molecule
-#include "physicalData.hpp"   // for PhysicalData
+#include "coulombPotential.hpp"   // for CoulombPotential
+#include "molecule.hpp"           // for Molecule
+#include "physicalData.hpp"       // for PhysicalData
 #include "potential.hpp"
 #include "simulationBox_kokkos.hpp"   // for SimulationBox implementation with Kokkos
-#include "coulombPotential.hpp"      // for CoulombPotential
 
 namespace simulationBox
 {
@@ -49,91 +47,84 @@ using namespace potential;
 inline void PotentialKokkos::
     calculateForces(simulationBox::SimulationBox &simBox, simulationBox::KokkosSimulationBox &kokkosSimBox, physicalData::PhysicalData &physicalData, simulationBox::CellList &)
 {
-    // Kokkos::initialize();
-    // {
-    //     // set total coulombic and non-coulombic energy
-    //     double totalCoulombEnergy    = 0.0;
-    //     double totalNonCoulombEnergy = 0.0;
+    // set total coulombic and non-coulombic energy
+    double totalCoulombEnergy    = 0.0;
+    double totalNonCoulombEnergy = 0.0;
 
-    //     // get number of atoms
-    //     const size_t numberOfAtoms = simBox.getNumberOfAtoms();
+    // get number of atoms
+    const size_t numberOfAtoms = simBox.getNumberOfAtoms();
 
-        kokkosSimBox.initializeForces();
-        kokkosSimBox.transferPositionsFromSimulationBox(simBox);
+    kokkosSimBox.initializeForces();
+    kokkosSimBox.transferPositionsFromSimulationBox(simBox);
 
-        auto atomTypes = kokkosSimBox.getAtomTypes().d_view;
-        auto molTypes  = kokkosSimBox.getMolTypes().d_view;
-        auto internalGlobalVDWTypes =
-            kokkosSimBox.getInternalGlobalVDWTypes().d_view;
+    auto atomTypes = kokkosSimBox.getAtomTypes().d_view;
+    auto molTypes  = kokkosSimBox.getMolTypes().d_view;
+    auto internalGlobalVDWTypes =
+        kokkosSimBox.getInternalGlobalVDWTypes().d_view;
 
-        auto positions      = kokkosSimBox.getPositions().d_view;
-        auto forces         = kokkosSimBox.getForces().d_view;
-        auto partialCharges = kokkosSimBox.getPartialCharges().d_view;
+    auto positions      = kokkosSimBox.getPositions().d_view;
+    auto forces         = kokkosSimBox.getForces().d_view;
+    auto partialCharges = kokkosSimBox.getPartialCharges().d_view;
 
-        Kokkos::parallel_reduce(
-            "Reduction",
-            numberOfAtoms,
-            KOKKOS_LAMBDA(
-                const size_t i,
-                double      &coulombEnergy,
-                double      &nonCoulombEnergy
-            ) {
+    Kokkos::parallel_reduce(
+        "Reduction",
+        numberOfAtoms,
+        KOKKOS_LAMBDA(
+            const size_t i,
+            double      &coulombEnergy,
+            double      &nonCoulombEnergy
+        ) {
+            const auto partialCharge_i = partialCharges(i);
+            const auto vdWType_i       = internalGlobalVDWTypes(i);
+            auto       force_i         = &forces(i, 0);
 
-                const auto partialCharge_i = partialCharges(i);
-                const auto vdWType_i       = internalGlobalVDWTypes(i);
-                auto force_i               = &forces(i, 0);
-
-                for (size_t j = 0; j < numberOfAtoms; ++j)
+            for (size_t j = 0; j < numberOfAtoms; ++j)
+            {
+                if (i == j)
                 {
-                    if (i == j)
-                    {
-                        continue;
-                    }
-
-                    double dxyz[3] = {
-                        positions(i, 0) - positions(j, 0),
-                        positions(i, 1) - positions(j, 1),
-                        positions(i, 2) - positions(j, 2)
-                    };
-
-                    auto normSquared = dxyz[0] * dxyz[0] + dxyz[1] * dxyz[1] + dxyz[2] * dxyz[2];
-                    auto distance   = Kokkos::sqrt(normSquared);
-
-
-                    if (distance < CoulombPotential::getCoulombRadiusCutOff())
-                    {
-                        continue;
-                    }
-
-                    const auto partialCharge_j = partialCharges(j);
-                    const auto vdWType_j       = internalGlobalVDWTypes(j);
-
-                    auto [coulombicEnergy, nonCoulombicEnergy] =
-                        PotentialKokkos::calculatePairEnergy
-                        (
-                            distance,
-                            dxyz,
-                            force_i,
-                            partialCharge_i,
-                            vdWType_i,
-                            partialCharge_j,
-                            vdWType_j
-                        );
-
-                    coulombEnergy    += coulombicEnergy;
-                    nonCoulombEnergy += nonCoulombicEnergy;
+                    continue;
                 }
-            },
-            totalCoulombEnergy,
-            totalNonCoulombEnergy
-        );
 
-        // set total coulombic and non-coulombic energy
-        physicalData.setCoulombEnergy(totalCoulombEnergy);
-        physicalData.setNonCoulombEnergy(totalNonCoulombEnergy);
+                double dxyz[3] = {
+                    positions(i, 0) - positions(j, 0),
+                    positions(i, 1) - positions(j, 1),
+                    positions(i, 2) - positions(j, 2)
+                };
 
-    }   // end of Kokkos scope
-    Kokkos::finalize();
+                auto normSquared =
+                    dxyz[0] * dxyz[0] + dxyz[1] * dxyz[1] + dxyz[2] * dxyz[2];
+                auto distance = Kokkos::sqrt(normSquared);
+
+                if (distance < CoulombPotential::getCoulombRadiusCutOff())
+                {
+                    continue;
+                }
+
+                const auto partialCharge_j = partialCharges(j);
+                const auto vdWType_j       = internalGlobalVDWTypes(j);
+
+                auto [coulombicEnergy, nonCoulombicEnergy] =
+                    PotentialKokkos::calculatePairEnergy(
+                        distance,
+                        dxyz,
+                        force_i,
+                        partialCharge_i,
+                        vdWType_i,
+                        partialCharge_j,
+                        vdWType_j
+                    );
+
+                coulombEnergy    += coulombicEnergy;
+                nonCoulombEnergy += nonCoulombicEnergy;
+            }
+        },
+        totalCoulombEnergy,
+        totalNonCoulombEnergy
+    );
+
+    // set total coulombic and non-coulombic energy
+    physicalData.setCoulombEnergy(totalCoulombEnergy);
+    physicalData.setNonCoulombEnergy(totalNonCoulombEnergy);
 }
 
 /**
@@ -145,7 +136,7 @@ KOKKOS_INLINE_FUNCTION
 Kokkos::pair<double, double> PotentialKokkos::calculatePairEnergy(
     const double distance,
     const double dxyz[3],
-    double *force_i,
+    double      *force_i,
     const double partialCharge_i,
     const size_t vdWType_i,
     const double partialCharge_j,
@@ -154,8 +145,6 @@ Kokkos::pair<double, double> PotentialKokkos::calculatePairEnergy(
 {
     auto coulombicEnergy    = 0.0;
     auto nonCoulombicEnergy = 0.0;
-
-
 
     return Kokkos::make_pair(coulombicEnergy, nonCoulombicEnergy);
 }
