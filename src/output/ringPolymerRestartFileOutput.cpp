1c830989db9da1e84853c139a0a5fb8cf308142b
Josef M. Gallmetzer <gallmetzer.josef@gmail.com>
2024-05-20 20:20:46 +0200

    Added buffer to restart writers and ringpoly trajectory writer

--- src/output/ringPolymerRestartFileOutput.cpp
+++ src/output/ringPolymerRestartFileOutput.cpp
@@ -22,15 +22,16 @@
 
 #include "ringPolymerRestartFileOutput.hpp"
 
+#include <format>    // for format
+#include <ostream>   // for basic_ostream, operator<<, flush, std
+#include <sstream>   // for ostringstream
+#include <string>    // for char_traits, operator<<
+#include <vector>    // for vector
+
 #include "molecule.hpp"              // for Molecule
 #include "ringPolymerSettings.hpp"   // for RingPolymerSettings
 #include "simulationBox.hpp"         // for SimulationBox
 #include "vector3d.hpp"              // for operator<<
-
-#include <format>    // for format
-#include <ostream>   // for basic_ostream, operator<<, flush, std
-#include <string>    // for char_traits, operator<<
-#include <vector>    // for vector
 
 using output::RingPolymerRestartFileOutput;
 
@@ -40,37 +41,56 @@ using output::RingPolymerRestartFileOutput;
  * @param simBox
  * @param step
  */
-void RingPolymerRestartFileOutput::write(std::vector<simulationBox::SimulationBox> &beads, const size_t step)
+void RingPolymerRestartFileOutput::write(
+    std::vector<simulationBox::SimulationBox> &beads,
+    const size_t                               step
+)
 {
+    std::ostringstream buffer;
+
     _fp.close();
 
     _fp.open(_fileName);
 
-    for (size_t i = 0; i < settings::RingPolymerSettings::getNumberOfBeads(); ++i)
+    for (size_t i = 0; i < settings::RingPolymerSettings::getNumberOfBeads();
+         ++i)
         for (const auto &molecule : beads[i].getMolecules())
         {
-            for (size_t j = 0, numberOfAtoms = molecule.getNumberOfAtoms(); j < numberOfAtoms; ++j)
+            for (size_t j = 0, numberOfAtoms = molecule.getNumberOfAtoms();
+                 j < numberOfAtoms;
+                 ++j)
             {
-                _fp << std::format("{:>5}{}\t", molecule.getAtomName(j), i + 1);
-                _fp << std::format("{:>5}\t", j + 1);
-                _fp << std::format("{:>5}\t", molecule.getMoltype());
+                buffer
+                    << std::format("{:>5}{}\t", molecule.getAtomName(j), i + 1);
+                buffer << std::format("{:>5}\t", j + 1);
+                buffer << std::format("{:>5}\t", molecule.getMoltype());
 
-                _fp << std::format("{:15.8f}\t{:15.8f}\t{:15.8f}\t",
-                                   molecule.getAtomPosition(j)[0],
-                                   molecule.getAtomPosition(j)[1],
-                                   molecule.getAtomPosition(j)[2]);
+                buffer << std::format(
+                    "{:15.8f}\t{:15.8f}\t{:15.8f}\t",
+                    molecule.getAtomPosition(j)[0],
+                    molecule.getAtomPosition(j)[1],
+                    molecule.getAtomPosition(j)[2]
+                );
 
-                _fp << std::format("{:19.8e}\t{:19.8e}\t{:19.8e}\t",
-                                   molecule.getAtomVelocity(j)[0],
-                                   molecule.getAtomVelocity(j)[1],
-                                   molecule.getAtomVelocity(j)[2]);
+                buffer << std::format(
+                    "{:19.8e}\t{:19.8e}\t{:19.8e}\t",
+                    molecule.getAtomVelocity(j)[0],
+                    molecule.getAtomVelocity(j)[1],
+                    molecule.getAtomVelocity(j)[2]
+                );
 
-                _fp << std::format("{:15.8f}\t{:15.8f}\t{:15.8f}",
-                                   molecule.getAtomForce(j)[0],
-                                   molecule.getAtomForce(j)[1],
-                                   molecule.getAtomForce(j)[2]);
+                buffer << std::format(
+                    "{:15.8f}\t{:15.8f}\t{:15.8f}",
+                    molecule.getAtomForce(j)[0],
+                    molecule.getAtomForce(j)[1],
+                    molecule.getAtomForce(j)[2]
+                );
 
-                _fp << '\n' << std::flush;
+                buffer << '\n';
             }
         }
+
+    // Write the buffer to the file
+    _fp << buffer.str();
+    _fp << std::flush;
 }
