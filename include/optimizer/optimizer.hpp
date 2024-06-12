1addafe5fe8d2ccc4ba5c3e9fe6e83ae324527a5
97gamjak <97gamjak@gmail.com>
2024-06-12 11:25:29 +0200

    feat: added new optimizer class
    this class will be used as base class for all optimizers that will be implemented in the future.

---
+++ include/optimizer/optimizer.hpp
@@ -0,0 +1,35 @@
+/*****************************************************************************
+<GPL_HEADER>
+
+    PQ
+    Copyright (C) 2023-now  Jakob Gamper
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+<GPL_HEADER>
+******************************************************************************/
+
+#ifndef _OPTIMIZER_HPP_
+
+#define _OPTIMIZER_HPP_
+
+namespace optimizer
+{
+    class Optimizer
+    {
+    public:
+    };
+} // namespace optimizer
+
+#endif // _OPTIMIZER_HPP_
