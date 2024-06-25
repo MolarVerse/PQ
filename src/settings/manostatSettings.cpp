2fe710b43015d531be0b6ba5f7f5df3df990796d
97gamjak <97gamjak@gmail.com>
2024-06-25 08:41:46 +0200

    refactor: Update ManostatSettings class formatting and naming

--- src/settings/manostatSettings.cpp
+++ src/settings/manostatSettings.cpp
@@ -24,24 +24,51 @@
 
 #include "stringUtilities.hpp"
 
-using settings::ManostatSettings;
+using namespace settings;
 
 /**
  * @brief return string of manostatType
  *
  * @param manostatType
  */
-std::string settings::string(const settings::ManostatType &manostatType)
+std::string settings::string(const ManostatType &manostatType)
 {
     switch (manostatType)
     {
-    case settings::ManostatType::BERENDSEN: return "berendsen";
+        case ManostatType::BERENDSEN: return "berendsen";
 
-    case settings::ManostatType::STOCHASTIC_RESCALING: return "stochastic_rescaling";
+        case ManostatType::STOCHASTIC_RESCALING: return "stochastic_rescaling";
 
-    default: return "none";
+        default: return "none";
     }
 }
+
+/**
+ * @brief return string of isotropy
+ *
+ * @param isotropy
+ */
+std::string settings::string(const Isotropy &isotropy)
+{
+    switch (isotropy)
+    {
+        case Isotropy::ISOTROPIC: return "isotropic";
+
+        case Isotropy::SEMI_ISOTROPIC: return "semi_isotropic";
+
+        case Isotropy::ANISOTROPIC: return "anisotropic";
+
+        case Isotropy::FULL_ANISOTROPIC: return "full_anisotropic";
+
+        default: return "isotropic";
+    }
+}
+
+/***************************
+ *                         *
+ * standard setter methods *
+ *                         *
+ ***************************/
 
 /**
  * @brief sets the manostatType to enum in settings
@@ -53,34 +80,23 @@ void ManostatSettings::setManostatType(const std::string_view &manostatType)
     const auto manostatTypeToLower = utilities::toLowerCopy(manostatType);
 
     if (manostatTypeToLower == "berendsen")
-        _manostatType = settings::ManostatType::BERENDSEN;
+        _manostatType = ManostatType::BERENDSEN;
 
     else if (manostatTypeToLower == "stochastic_rescaling")
-        _manostatType = settings::ManostatType::STOCHASTIC_RESCALING;
+        _manostatType = ManostatType::STOCHASTIC_RESCALING;
 
     else
-        _manostatType = settings::ManostatType::NONE;
+        _manostatType = ManostatType::NONE;
 }
 
 /**
- * @brief return string of isotropy
+ * @brief sets the manostatType to enum in settings
  *
- * @param isotropy
+ * @param manostatType
  */
-std::string settings::string(const settings::Isotropy &isotropy)
+void ManostatSettings::setManostatType(const ManostatType &manostatType)
 {
-    switch (isotropy)
-    {
-    case settings::Isotropy::ISOTROPIC: return "isotropic";
-
-    case settings::Isotropy::SEMI_ISOTROPIC: return "semi_isotropic";
-
-    case settings::Isotropy::ANISOTROPIC: return "anisotropic";
-
-    case settings::Isotropy::FULL_ANISOTROPIC: return "full_anisotropic";
-
-    default: return "isotropic";
-    }
+    _manostatType = manostatType;
 }
 
 /**
@@ -93,17 +109,152 @@ void ManostatSettings::setIsotropy(const std::string_view &isotropy)
     const auto isotropyToLower = utilities::toLowerCopy(isotropy);
 
     if (isotropyToLower == "isotropic")
-        _isotropy = settings::Isotropy::ISOTROPIC;
+        _isotropy = Isotropy::ISOTROPIC;
 
     else if (isotropyToLower == "semi_isotropic")
-        _isotropy = settings::Isotropy::SEMI_ISOTROPIC;
+        _isotropy = Isotropy::SEMI_ISOTROPIC;
 
     else if (isotropyToLower == "anisotropic")
-        _isotropy = settings::Isotropy::ANISOTROPIC;
+        _isotropy = Isotropy::ANISOTROPIC;
 
     else if (isotropyToLower == "full_anisotropic")
-        _isotropy = settings::Isotropy::FULL_ANISOTROPIC;
+        _isotropy = Isotropy::FULL_ANISOTROPIC;
 
     else
-        _isotropy = settings::Isotropy::ISOTROPIC;
+        _isotropy = Isotropy::ISOTROPIC;
 }
+
+/**
+ * @brief sets the isotropy to enum in settings
+ *
+ * @param isotropy
+ */
+void ManostatSettings::setIsotropy(const Isotropy &isotropy)
+{
+    _isotropy = isotropy;
+}
+
+/**
+ * @brief sets the pressureSet to bool in settings
+ *
+ * @param pressureSet
+ */
+void ManostatSettings::setPressureSet(const bool pressureSet)
+{
+    _isPressureSet = pressureSet;
+}
+
+/**
+ * @brief sets the targetPressure to double in settings
+ *
+ * @param target
+ */
+void ManostatSettings::setTargetPressure(const double targetPressure)
+{
+    _targetPressure = targetPressure;
+}
+
+/**
+ * @brief sets the tauManostat to double in settings
+ *
+ * @param tauManostat
+ */
+void ManostatSettings::setTauManostat(const double tauManostat)
+{
+    _tauManostat = tauManostat;
+}
+
+/**
+ * @brief sets the compressibility to double in settings
+ *
+ * @param compressibility
+ */
+void ManostatSettings::setCompressibility(const double compressibility)
+{
+    _compressibility = compressibility;
+}
+
+/**
+ * @brief sets the 2D isotropic axes to vector<size_t> in settings
+ *
+ * @param indices
+ */
+void ManostatSettings::set2DIsotropicAxes(const std::vector<size_t> &indices)
+{
+    _2DIsotropicAxes = indices;
+}
+
+/**
+ * @brief sets the 2D anisotropic axis to size_t in settings
+ *
+ * @param index
+ */
+void ManostatSettings::set2DAnisotropicAxis(const size_t index)
+{
+    _2DAnisotropicAxis = index;
+}
+
+/***************************
+ *                         *
+ * standard getter methods *
+ *                         *
+ ***************************/
+
+/**
+ * @brief get the manostatType
+ *
+ * @return ManostatType
+ */
+ManostatType ManostatSettings::getManostatType() { return _manostatType; }
+
+/**
+ * @brief get the isotropy
+ *
+ * @return Isotropy
+ */
+Isotropy ManostatSettings::getIsotropy() { return _isotropy; }
+
+/**
+ * @brief get if pressure is set
+ *
+ * @return bool
+ */
+bool ManostatSettings::isPressureSet() { return _isPressureSet; }
+
+/**
+ * @brief get the target pressure
+ *
+ * @return double
+ */
+double ManostatSettings::getTargetPressure() { return _targetPressure; }
+
+/**
+ * @brief get the tauManostat
+ *
+ * @return double
+ */
+double ManostatSettings::getTauManostat() { return _tauManostat; }
+
+/**
+ * @brief get the compressibility
+ *
+ * @return double
+ */
+double ManostatSettings::getCompressibility() { return _compressibility; }
+
+/**
+ * @brief get the 2D isotropic axes
+ *
+ * @return std::vector<size_t>
+ */
+std::vector<size_t> ManostatSettings::get2DIsotropicAxes()
+{
+    return _2DIsotropicAxes;
+}
+
+/**
+ * @brief get the 2D anisotropic axis
+ *
+ * @return size_t
+ */
+size_t ManostatSettings::get2DAnisotropicAxis() { return _2DAnisotropicAxis; }
