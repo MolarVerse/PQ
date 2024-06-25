86334ed5f64428577775604cb29a7cadf0ecc220
97gamjak <97gamjak@gmail.com>
2024-06-25 12:13:48 +0200

    refactor: Update default values and variable names in config/defaults.hpp

--- include/settings/thermostatSettings.hpp
+++ include/settings/thermostatSettings.hpp
@@ -66,22 +66,22 @@ namespace settings
         static inline bool _isStartTemperatureSet = false;
         static inline bool _isEndTemperatureSet   = false;
 
-        static inline size_t _noseHooverChainLength =
-            defaults::_NOSE_HOOVER_CHAIN_LENGTH_DEFAULT_;   // 3
+        // clang-format off
+        static inline size_t _nhChainLength            = defaults::_NH_CHAIN_LENGTH_DEFAULT_;
         static inline size_t _temperatureRampSteps     = 0;
         static inline size_t _temperatureRampFrequency = 1;
+        // clang-format on
 
         static inline double _targetTemperature;
         static inline double _actualTargetTemperature;   // for reset kinetics
         static inline double _startTemperature;
         static inline double _endTemperature;
 
-        static inline double _relaxationTime =
-            defaults::_BERENDSEN_THERMOSTAT_RELAXATION_TIME_;   // 0.1 ps
-        static inline double _friction =
-            defaults::_LANGEVIN_THERMOSTAT_FRICTION_;   // 10.0 ps^-1
-        static inline double _noseHooverCouplingFrequency =
-            defaults::_NOSE_HOOVER_COUPLING_FREQUENCY_;   // 1.0e6
+        // clang-format off
+        static inline double _relaxationTime = defaults::_BERENDSEN_THERMOSTAT_RELAX_TIME_;
+        static inline double _friction       = defaults::_LANGEVIN_THERMOSTAT_FRICTION_;
+        static inline double _nhCouplingFreq = defaults::_NH_COUPLING_FREQ_;
+        // clang-format on
 
         static inline std::map<size_t, double> _chi;
         static inline std::map<size_t, double> _zeta;
@@ -90,188 +90,56 @@ namespace settings
         ThermostatSettings()  = default;
         ~ThermostatSettings() = default;
 
-        static void setThermostatType(const std::string_view &thermostatType);
+        static auto addChi(const size_t index, const double chi);
+        static auto addZeta(const size_t index, const double zeta);
 
         /***************************
          * standard setter methods *
          ***************************/
 
-        // ThermostatType setters
+        static void setThermostatType(const std::string_view &thermostatType);
+        static void setThermostatType(const ThermostatType &thermostatType);
 
-        static void setThermostatType(const ThermostatType &thermostatType)
-        {
-            _thermostatType = thermostatType;
-        }
+        static void setTemperatureSet(const bool);
+        static void setStartTemperatureSet(const bool);
+        static void setEndTemperatureSet(const bool);
+        static void setTargetTemperature(const double);
+        static void setActualTargetTemperature(const double);
+        static void setStartTemperature(const double);
+        static void setEndTemperature(const double);
 
-        // size_t setters
+        static void setTemperatureRampSteps(const size_t);
+        static void setTemperatureRampFrequency(const size_t);
 
-        static void setNoseHooverChainLength(const size_t length)
-        {
-            _noseHooverChainLength = length;
-        }
-
-        static void setTemperatureRampSteps(const size_t steps)
-        {
-            _temperatureRampSteps = steps;
-        }
-
-        static void setTemperatureRampFrequency(const size_t frequency)
-        {
-            _temperatureRampFrequency = frequency;
-        }
-
-        // bool setters
-
-        static void setTemperatureSet(const bool temperatureSet)
-        {
-            _isTemperatureSet = temperatureSet;
-        }
-
-        static void setStartTemperatureSet(const bool startTemperatureSet)
-        {
-            _isStartTemperatureSet = startTemperatureSet;
-        }
-
-        static void setEndTemperatureSet(const bool endTemperatureSet)
-        {
-            _isEndTemperatureSet = endTemperatureSet;
-        }
-
-        // double setters
-
-        static void setTargetTemperature(const double targetTemperature)
-        {
-            _targetTemperature = targetTemperature;
-            setTemperatureSet(true);
-            setActualTargetTemperature(targetTemperature);
-        }
-
-        static void setActualTargetTemperature(
-            const double actualTargetTemperature
-        )
-        {
-            _actualTargetTemperature = actualTargetTemperature;
-        }
-
-        static void setStartTemperature(const double startTemperature)
-        {
-            _startTemperature = startTemperature;
-            setStartTemperatureSet(true);
-        }
-
-        static void setEndTemperature(const double endTemperature)
-        {
-            _endTemperature = endTemperature;
-            setEndTemperatureSet(true);
-        }
-
-        static void setRelaxationTime(const double relaxationTime)
-        {
-            _relaxationTime = relaxationTime;
-        }
-
-        static void setFriction(const double friction) { _friction = friction; }
-
-        static void setNoseHooverCouplingFrequency(const double frequency)
-        {
-            _noseHooverCouplingFrequency = frequency;
-        }
-
-        static auto addChi(const size_t index, const double chi)
-        {
-            return _chi.try_emplace(index, chi);
-        }
-
-        static auto addZeta(const size_t index, const double zeta)
-        {
-            return _zeta.try_emplace(index, zeta);
-        }
+        static void setRelaxationTime(const double);
+        static void setFriction(const double);
+        static void setNoseHooverChainLength(const size_t);
+        static void setNoseHooverCouplingFrequency(const double);
 
         /***************************
          * standard getter methods *
          ***************************/
 
-        // size_t getters
+        [[nodiscard]] static ThermostatType getThermostatType();
 
-        [[nodiscard]] static size_t getNoseHooverChainLength()
-        {
-            return _noseHooverChainLength;
-        }
+        [[nodiscard]] static size_t getNoseHooverChainLength();
+        [[nodiscard]] static size_t getTemperatureRampSteps();
+        [[nodiscard]] static size_t getTemperatureRampFrequency();
 
-        [[nodiscard]] static size_t getTemperatureRampSteps()
-        {
-            return _temperatureRampSteps;
-        }
+        [[nodiscard]] static bool isTemperatureSet();
+        [[nodiscard]] static bool isStartTemperatureSet();
+        [[nodiscard]] static bool isEndTemperatureSet();
 
-        [[nodiscard]] static size_t getTemperatureRampFrequency()
-        {
-            return _temperatureRampFrequency;
-        }
+        [[nodiscard]] static double getTargetTemperature();
+        [[nodiscard]] static double getActualTargetTemperature();
+        [[nodiscard]] static double getStartTemperature();
+        [[nodiscard]] static double getEndTemperature();
+        [[nodiscard]] static double getRelaxationTime();
+        [[nodiscard]] static double getFriction();
+        [[nodiscard]] static double getNoseHooverCouplingFrequency();
 
-        // ThermostatType getters
-
-        [[nodiscard]] static ThermostatType getThermostatType()
-        {
-            return _thermostatType;
-        }
-
-        // bool getters
-
-        [[nodiscard]] static bool isTemperatureSet()
-        {
-            return _isTemperatureSet;
-        }
-
-        [[nodiscard]] static bool isStartTemperatureSet()
-        {
-            return _isStartTemperatureSet;
-        }
-
-        [[nodiscard]] static bool isEndTemperatureSet()
-        {
-            return _isEndTemperatureSet;
-        }
-
-        // double getters
-
-        [[nodiscard]] static double getTargetTemperature()
-        {
-            return _targetTemperature;
-        }
-
-        [[nodiscard]] static double getActualTargetTemperature()
-        {
-            return _actualTargetTemperature;
-        }
-
-        [[nodiscard]] static double getStartTemperature()
-        {
-            return _startTemperature;
-        }
-
-        [[nodiscard]] static double getEndTemperature()
-        {
-            return _endTemperature;
-        }
-
-        [[nodiscard]] static double getRelaxationTime()
-        {
-            return _relaxationTime;
-        }
-
-        [[nodiscard]] static double getFriction() { return _friction; }
-
-        [[nodiscard]] static double getNoseHooverCouplingFrequency()
-        {
-            return _noseHooverCouplingFrequency;
-        }
-
-        [[nodiscard]] static std::map<size_t, double> getChi() { return _chi; }
-
-        [[nodiscard]] static std::map<size_t, double> getZeta()
-        {
-            return _zeta;
-        }
+        [[nodiscard]] static std::map<size_t, double> getChi();
+        [[nodiscard]] static std::map<size_t, double> getZeta();
     };
 }   // namespace settings
 
