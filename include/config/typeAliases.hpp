e2b6ad68ba1e00c8dd4c3575c729b8e3956b34e3
Jakob Gamper <97gamjak@gmail.com>
2024-07-02 00:39:45 +0200

    refactor: Clean up QMMMSetup

--- include/config/typeAliases.hpp
+++ include/config/typeAliases.hpp
@@ -116,10 +116,11 @@ namespace output
 
 namespace engine
 {
-    class Engine;       // forward declaration
-    class MDEngine;     // forward declaration
-    class OptEngine;    // forward declaration
-    class QMMDEngine;   // forward declaration
+    class Engine;         // forward declaration
+    class MDEngine;       // forward declaration
+    class OptEngine;      // forward declaration
+    class QMMDEngine;     // forward declaration
+    class QMMMMDEngine;   // forward declaration
 
 }   // namespace engine
 
@@ -175,10 +176,11 @@ namespace pq
      * engine namespace *
      ********************/
 
-    using Engine     = engine::Engine;
-    using MDEngine   = engine::MDEngine;
-    using OptEngine  = engine::OptEngine;
-    using QMMDEngine = engine::QMMDEngine;
+    using Engine       = engine::Engine;
+    using MDEngine     = engine::MDEngine;
+    using OptEngine    = engine::OptEngine;
+    using QMMDEngine   = engine::QMMDEngine;
+    using QMMMMDEngine = engine::QMMMMDEngine;
 
     using UniqueEngine = std::unique_ptr<Engine>;
 
