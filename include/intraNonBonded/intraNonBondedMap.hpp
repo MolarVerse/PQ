bb8fbd9aefa1f9779a2c5d09c3fb4c2ea85e54c4
97gamjak <97gamjak@gmail.com>
2024-07-03 15:12:13 +0200

    refactor: Clean up libIntraNonBonded

--- include/intraNonBonded/intraNonBondedMap.hpp
+++ include/intraNonBonded/intraNonBondedMap.hpp
@@ -24,66 +24,57 @@
 
 #define _INTRA_NON_BONDED_MAP_HPP_
 
-#include "intraNonBondedContainer.hpp"   // for IntraNonBondedContainer
-#include "vector3d.hpp"                  // for Vec3D
-
 #include <cstddef>   // for size_t
 #include <utility>   // for pair
 #include <vector>    // for vector
 
-namespace simulationBox
-{
-    class SimulationBox;   // forward declaration
-    class Molecule;        // forward declaration
-}   // namespace simulationBox
-
-namespace physicalData
-{
-    class PhysicalData;   // forward declaration
-}
-
-namespace potential
-{
-    class CoulombPotential;      // forward declaration
-    class NonCoulombPotential;   // forward declaration
-}   // namespace potential
+#include "intraNonBondedContainer.hpp"   // for IntraNonBondedContainer
+#include "typeAliases.hpp"
+#include "vector3d.hpp"   // for Vec3D
 
 namespace intraNonBonded
 {
     /**
      * @class IntraNonBondedMap
      *
-     * @brief defines a map for a single molecule to its intra non bonded interactions
+     * @brief defines a map for a single molecule to its intra non bonded
+     * interactions
      */
     class IntraNonBondedMap
     {
-      private:
-        simulationBox::Molecule *_molecule;
+       private:
+        pq::Molecule            *_molecule;
         IntraNonBondedContainer *_intraNonBondedContainer;
 
-      public:
-        explicit IntraNonBondedMap(simulationBox::Molecule *molecule, IntraNonBondedContainer *intraNonBondedType)
-            : _molecule(molecule), _intraNonBondedContainer(intraNonBondedType){};
+       public:
+        explicit IntraNonBondedMap(
+            pq::Molecule            *molecule,
+            IntraNonBondedContainer *intraNonBondedType
+        );
 
-        void calculate(const potential::CoulombPotential *,
-                       potential::NonCoulombPotential *,
-                       const simulationBox::SimulationBox &,
-                       physicalData::PhysicalData &) const;
+        void calculate(
+            const pq::CoulombPot *coulPot,
+            pq::NonCoulombPot    *nonCoulPot,
+            const pq::SimBox     &simBox,
+            pq::PhysicalData     &data
+        ) const;
 
-        [[nodiscard]] std::pair<double, double> calculateSingleInteraction(const size_t                atomIndex1,
-                                                                           const int                   atomIndex2AsInt,
-                                                                           const linearAlgebra::Vec3D &box,
-                                                                           physicalData::PhysicalData &,
-                                                                           const potential::CoulombPotential *,
-                                                                           potential::NonCoulombPotential *) const;
+        [[nodiscard]] std::pair<double, double> calculateSingleInteraction(
+            const size_t          atomIdx1,
+            const int             atomIdx2,
+            const pq::Vec3D      &box,
+            pq::PhysicalData     &data,
+            const pq::CoulombPot *coulPot,
+            pq::NonCoulombPot    *nonCoulPot
+        ) const;
 
         /***************************
          * standard getter methods *
          ***************************/
 
-        [[nodiscard]] IntraNonBondedContainer      *getIntraNonBondedType() const { return _intraNonBondedContainer; }
-        [[nodiscard]] simulationBox::Molecule      *getMolecule() const { return _molecule; }
-        [[nodiscard]] std::vector<std::vector<int>> getAtomIndices() const { return _intraNonBondedContainer->getAtomIndices(); }
+        [[nodiscard]] IntraNonBondedContainer *getIntraNonBondedType() const;
+        [[nodiscard]] pq::Molecule            *getMolecule() const;
+        [[nodiscard]] std::vector<std::vector<int>> getAtomIndices() const;
     };
 
 }   // namespace intraNonBonded
