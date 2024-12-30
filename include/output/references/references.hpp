e3ac9c300b7d9c80cc3efa4614bfcd173963fdb9
Armin Penz <armin.penz@uibk.ac.at>
2024-12-30 10:30:55 +0100

    DFTB+ citation file added

--- include/output/references/references.hpp
+++ include/output/references/references.hpp
@@ -27,11 +27,14 @@
 namespace references
 {
     // clang-format off
+    // Thermostats
     static constexpr char _BERENDSEN_FILE_[]            = "berendsen.ref";
     static constexpr char _VELOCITY_RESCALING_FILE_[]   = "velocity_rescaling.ref";
     static constexpr char _NOSE_HOOVER_CHAIN_FILE_[]    = "nose_hoover_chain.ref";
     static constexpr char _LANGEVIN_FILE_[]             = "langevin.ref";
     static constexpr char _STOCHASTIC_RESCALING_FILE_[] = "stochastic_rescaling.ref";
+    // QM Programs
+    static constexpr char _DFTBPLUS_FILE_[]             = "dftbplus.ref";
     // clang-format on
 
 }   // namespace references
