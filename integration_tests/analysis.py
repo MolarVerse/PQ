5842273b1775531759080313cb4fee3ecc4734d9
Josef M. Gallmetzer <gallmetzer.josef@gmail.com>
2024-12-30 22:37:31 +0100

    test: increase absolute tolerance for energy comparison in analysis tests

--- integration_tests/analysis.py
+++ integration_tests/analysis.py
@@ -61,7 +61,7 @@ def check_pq_output(base_name, folder_name, ref_folder="ref_data"):
     energy = reader.read()
     ref_energy = ref_reader.read()
 
-    assert np.allclose(energy.data[:-2], ref_energy.data[:-2])
+    assert np.allclose(energy.data[:-2], ref_energy.data[:-2], atol=1e-6)
 
     assert energy.info_given
     assert energy.info == ref_energy.info
