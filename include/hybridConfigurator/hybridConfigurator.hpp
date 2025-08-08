79c30fafb25fa3c3fd8b599cbdaea7ca5c269bd0
Armin Penz <Armin.Penz@student.uibk.ac.at>
2025-08-08 13:10:14 +0200

    feature: add hybridConfigurator class

---
+++ include/hybridConfigurator/hybridConfigurator.hpp
@@ -0,0 +1,50 @@
+/*****************************************************************************
+<GPL_HEADER>
+
+    PQ
+    Copyright (C) 2023-now  Jakob Gamper
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+<GPL_HEADER>
+******************************************************************************/
+
+#ifndef _HYBRID_CONFIGURATOR_HPP_
+
+#define _HYBRID_CONFIGURATOR_HPP_
+
+#include "simulationBox.hpp"
+#include "typeAliases.hpp"
+
+namespace configurator
+{
+
+    class HybridConfigurator
+    {
+       private:
+        pq::Vec3D innerRegionCenter = {0.0, 0.0, 0.0};
+
+       public:
+        virtual ~HybridConfigurator() = default;
+
+        virtual void run(pq::SimBox &) = 0;
+
+        void calculateInnerRegionCenter(pq::SimBox &);
+        void shiftAtomsToInnerRegionCenter(pq::SimBox &);
+        void shiftAtomsBackToInitialPositions(pq::SimBox &);
+    };
+
+}   // namespace configurator
+
+#endif   // _HYBRID_CONFIGURATOR_HPP_
