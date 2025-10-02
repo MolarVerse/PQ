e15caf493d276cb234cd1ff70896f36dbdc89b30
Armin Penz <Armin.Penz@student.uibk.ac.at>
2025-10-02 14:15:33 +0200

    feat: include charge calculation in pyscf scripts

--- src/QM/scripts/pyscf_mp2.py
+++ src/QM/scripts/pyscf_mp2.py
@@ -12,11 +12,27 @@ mol.atom = "coords.xyz"
 mol.basis = basis
 mol.build()
 
-calc = mp.UMP2(scf.UHF(mol)).nuc_grad_method().as_scanner()
+# First run the SCF calculation
+uhf = scf.UHF(mol)
+uhf.run()
 
+# Then run the MP2 calculation using the converged SCF
+mp2 = mp.UMP2(uhf)
+mp2.run()
+
+# Get Mulliken charges from the converged SCF (not MP2)
+charges = uhf.mulliken_pop()[1]  # [1] gives the charges, [0] gives the individual orbital populations
+
+# Now run the gradient calculation
+calc = mp2.nuc_grad_method().as_scanner()
 e_tot, grad = calc(mol)
 
+# Write result to the files
 with open('qm_forces', 'w') as f:
     print(e_tot, file=f)
     for i in range(len(grad)):
         print(grad[i][0], grad[i][1], grad[i][2], file=f)
+
+with open('qm_charges', 'w') as f:
+    for i, charge in enumerate(charges, 1):
+        print(f"{i:4d}    {charge:13.10f}", file=f)
