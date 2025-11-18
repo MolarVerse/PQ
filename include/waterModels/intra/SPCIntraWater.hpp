676f7592764a82b1355a61befca679c52fa15fa2
Armin Penz <armin.penz@student.uibk.ac.at>
2025-11-18 11:44:27 +0100

    feat: Add SPC intra water model files

---
+++ include/waterModels/intra/SPCIntraWater.hpp
@@ -0,0 +1,65 @@
+/*****************************************************************************
+<GPL_HEADER>
+
+    PQ
+    Copyright (C) 2023-now  Jakob Gamper
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+<GPL_HEADER>
+******************************************************************************/
+
+#ifndef _SPC_INTRA_WATER_HPP_
+#define _SPC_INTRA_WATER_HPP_
+
+#include "intraWater.hpp"
+#include "simulationBox.hpp"   // for SimulationBox
+#include "typeAliases.hpp"
+
+namespace waterModels
+{
+    template <class Derived>
+    class SPCIntraWater : public IntraWater
+    {
+       public:
+        virtual void calculate(pq::SimBox& box) override;
+    };
+
+    class SPCFwIntraParam : public SPCIntraWater<SPCFwIntraParam>
+    {
+       private:
+        static constexpr double _eqOHDistance          = 1.012;
+        static constexpr double _eqHOHAngle            = 113.24;
+        static constexpr double _forceConstantOHBond   = 1059.162;
+        static constexpr double _forceConstantHOHAngle = 75.9;
+
+        friend class SPCIntraWater<SPCFwIntraParam>;
+    };
+
+    class qSPCFwIntraParam : public SPCIntraWater<qSPCFwIntraParam>
+    {
+       private:
+        static constexpr double _eqOHDistance          = 1.0;
+        static constexpr double _eqHOHAngle            = 112.0;
+        static constexpr double _forceConstantOHBond   = 1059.162;
+        static constexpr double _forceConstantHOHAngle = 75.9;
+
+        friend class SPCIntraWater<qSPCFwIntraParam>;
+    };
+
+}   // namespace waterModels
+
+#include "SPCIntraWater.tpp.hpp"   // DO NOT MOVE THIS LINE
+
+#endif   //  _SPC_INTRA_WATER_HPP_
