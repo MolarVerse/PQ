2b4f3c9adc4b9db5bf7ee50e4d856293c6d82b4e
Armin Penz <Armin.Penz@student.uibk.ac.at>
2026-08-04 15:15:39 +0200

    fix: set default smoothing_method to hotspot

--- include/settings/hybridSettings.hpp
+++ include/settings/hybridSettings.hpp
@@ -37,7 +37,6 @@ namespace settings
      */
     enum class SmoothingMethod
     {
-        NONE,
         HOTSPOT,
         EXACT
     };
@@ -62,7 +61,7 @@ namespace settings
         static inline double _smoothingRegionThickness = 0.0;
         static inline double _pointChargeThickness     = 0.0;
 
-        static inline SmoothingMethod _smoothing = SmoothingMethod::NONE;
+        static inline SmoothingMethod _smoothing = SmoothingMethod::HOTSPOT;
 
        public:
         /********************
